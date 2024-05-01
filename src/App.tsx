60847675c87037afb0e13d90ee847d2775cab95d
tcocco20 <theo@coccoscoding.dev>
2024-05-01 10:25:53 -0700

    Initial commit, set up project with nextui and tailwind

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { Button } from "@nextui-org/react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="container">
+        <h1 className="underline">Test Header</h1>
+        <Button>Click me</Button>
+      </div>
+    </>
+  );
+}
+
+export default App;

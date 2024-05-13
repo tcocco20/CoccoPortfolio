cb89743403404b6aaa5eed000730dfaa6c9bf2ee
tcocco20 <theo@coccoscoding.dev>
2024-05-13 12:23:59 -0700

    started setting up parallax

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,52 @@
-import { Button } from "@nextui-org/react";
-import "./App.css";
+import { Card, CardBody, CardFooter, CardHeader } from "@nextui-org/react";
+import { Parallax, ParallaxLayer } from "@react-spring/parallax";
 
 function App() {
   return (
-    <>
-      <div className="container">
-        <h1 className="underline">Test Header</h1>
-        <Button>Click me</Button>
-      </div>
-    </>
+    <div className="bg-blue-400 h-screen w-screen">
+      <Parallax pages={4}>
+        <ParallaxLayer speed={1} factor={1.5}>
+          <div className="h-full w-full bg-black"></div>
+        </ParallaxLayer>
+        <ParallaxLayer offset={1} speed={0.5}>
+          <div className="flex gap-5 justify-center">
+            <Card>
+              <CardHeader>
+                <h1>Layer 2 Card</h1>
+              </CardHeader>
+              <CardBody>
+                <p>Layer 2 Card Content</p>
+              </CardBody>
+              <CardFooter>
+                <p>Layer 2 Card Footer</p>
+              </CardFooter>
+            </Card>
+            <Card>
+              <CardHeader>
+                <h1>Layer 2 Card</h1>
+              </CardHeader>
+              <CardBody>
+                <p>Layer 2 Card Content</p>
+              </CardBody>
+              <CardFooter>
+                <p>Layer 2 Card Footer</p>
+              </CardFooter>
+            </Card>
+            <Card>
+              <CardHeader>
+                <h1>Layer 2 Card</h1>
+              </CardHeader>
+              <CardBody>
+                <p>Layer 2 Card Content</p>
+              </CardBody>
+              <CardFooter>
+                <p>Layer 2 Card Footer</p>
+              </CardFooter>
+            </Card>
+          </div>
+        </ParallaxLayer>
+      </Parallax>
+    </div>
   );
 }
 

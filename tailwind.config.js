294c8370a779ba99c87e8b1d1d7740395347aee1
tcocco20 <theo@coccoscoding.dev>
2024-05-14 10:32:45 -0700

    added star color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,14 @@ export default {
     "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        starColor1: "#f6f0fe",
+        starColor2: "#dfabca",
+        starColor3: "#e9c1d4",
+        starColor4: "#593a82",
+      },
+    },
   },
   darkMode: "class",
   plugins: [nextui()],

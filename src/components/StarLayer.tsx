d413fa68edd6faf21ba24bb9e43f2f7345bf1267
tcocco20 <theo@coccoscoding.dev>
2024-06-26 14:12:50 -0700

    still trying to figure out how to change state in child element from parent, may try imperative handle.

--- src/components/StarLayer.tsx
+++ src/components/StarLayer.tsx
@@ -1,40 +1,46 @@
-import { useEffect } from "react";
+import { type ReactElement, useEffect } from "react";
 import Star from "./Star";
 import useAppStore from "../store/appStore";
+import Utils from "../utils";
 
 interface StarLayerProps {
   position: "front" | "middle" | "back";
 }
 
 const StarLayer = ({ position }: StarLayerProps) => {
-  const stars = useAppStore((state) => state.starRefs);
+  if (position !== "front") {
+    const generateStars = () => {
+      const stars: ReactElement[] = [];
+      if (position === "middle") {
+        for (let i = 0; i < innerWidth / 15; i++)
+          stars.push(<Star key={`medium ${i}`} size="medium" />);
+      } else {
+        for (let i = 0; i < innerWidth / 5; i++)
+          stars.push(<Star key={`small ${i}`} size="small" />);
+      }
+
+      return stars;
+    };
+
+    return (
+      <section className="h-screen w-screen absolute top-0 left-0 pointer-events-none z-50">
+        {generateStars()}
+      </section>
+    );
+  }
+  
+  const stars: ReactElement[] = [];
+  const starRefs = useAppStore((state) => state.starRefs);
   useEffect(() => {
     if (position === "front") {
-      console.log(stars);
+      window.addEventListener("mousemove", (e) => {
+        stars.forEach((s, i) => {
+          if (Utils.calcDistance(s, e) < innerWidth / 18) {
+          }
+        });
+      });
     }
   }, [position, stars]);
-
-  const generateStars = () => {
-    const stars = [];
-    if (position === "front") {
-      for (let i = 0; i < innerWidth / 30; i++)
-        stars.push(<Star lightStar={false} key={`large ${i}`} size="large" />);
-    } else if (position === "middle") {
-      for (let i = 0; i < innerWidth / 15; i++)
-        stars.push(<Star key={`medium ${i}`} size="medium" />);
-    } else {
-      for (let i = 0; i < innerWidth / 5; i++)
-        stars.push(<Star key={`small ${i}`} size="small" />);
-    }
-
-    return stars;
-  };
-
-  return (
-    <section className="h-screen w-screen absolute top-0 left-0 pointer-events-none z-50">
-      {generateStars()}
-    </section>
-  );
 };
 
 export default StarLayer;
